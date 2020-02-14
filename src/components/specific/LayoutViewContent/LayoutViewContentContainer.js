6515f5c87b851bd8fa222fdaf6f691f3f8762507
Sasha <oleksandr.maslyennikov@arnia.ro>
2020-02-14 10:53:10 +0200

    some changes

--- src/components/specific/LayoutViewContent/LayoutViewContentContainer.js
+++ src/components/specific/LayoutViewContent/LayoutViewContentContainer.js
@@ -1,28 +1,23 @@
 import React from 'react';
+import { map, range } from "lodash";
 
 import LayoutViewContent from './LayoutViewContent';
-import { map, range } from "lodash";
 import { layoutsNumber } from "../../../config/config";
 
-const generateLayout = () => {
-    return map(new Array(layoutsNumber), (item, i) => ({
-            x: i * 4 % 12,
-            y: 4,
-            w: 3.8,
-            h: 5,
-            i: i.toString()
-        }
-    ));
-};
+const generateLayout = () => map(new Array(layoutsNumber), (item, i) => ({
+        x: i * 4 % 12,
+        y: 4,
+        w: 3.8,
+        h: 5,
+        i: i.toString()
+    }
+));
 
-const generateDOM = () => {
-    return map(range(layoutsNumber), (i) => (
-            <div key={i} style={{ border: '1px solid #3f51b5', margin: '10px' }}>
-                <span>{i}</span>
-            </div>
-        )
-    );
-};
+const generateDOM = () => map(range(layoutsNumber), (i) => (
+    <div key={i} style={{ border: '1px solid #3f51b5', margin: '10px' }}>
+        <span>{i}</span>
+    </div>
+));
 
 const LayoutViewContentContainer = () => {
     const layout = generateLayout();
--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,13 @@ export const translations = {
 export const layoutsNumber = 6;
 
 export const configurationLayouts = [
-    [],
+    [
+        { x: 0, y: 0, w: 12, h: 1, i: '0' },
+        { x: 0, y: 0, w: 6, h: 2, i: '0' },
+        { x: 0, y: 0, w: 6, h: 2, i: '0' },
+        { x: 0, y: 0, w: 12, h: 2, i: '0' },
+        { x: 0, y: 0, w: 12, h: 1, i: '0' },
+    ],
     [],
     [],
     [],
